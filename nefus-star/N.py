339e7f5fff83c529cf1e1edc024e33164fbe5e8d
shojoinfo <hello111353@gmail.com>
2019-08-22 23:22:48 +0900

    n clear

--- nefus-star/N.py
+++ nefus-star/N.py
@@ -2,9 +2,17 @@ a=int(input())
 for i in range(0,a):
     for j in  range(0,a-i):
         print(" ",end='')
+    print("*",end='')
     if i==0:
+        for i in range(0,((a-1))*2-1):
+            print(" ",end='')
         print("*")
         continue
-    for j in range(0,i):
+    for j in range(0,i*2-1):
+        print(" ",end='')
+    print("*",end='')
+    if i==a-1:
+        continue
+    for i in range(0,((a-1)-i)*2-1):
         print(" ",end='')
     print("*")
